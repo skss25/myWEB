47ec88aef4c2ad390841ee8c5982fce6221d7bbb
skss25 <skss25@naver.com>
2018-11-21 08:16:10 +0900

    Add files via upload

---
+++ Reservation/Javascript/Basics/5.5/Keyword.js
@@ -0,0 +1,21 @@
+//변수 선언
+var x = 5 + 6;
+
+//함수의 선언
+function foo(arg){
+    //함수 종류 및 값의 반환
+    return ++arg;
+}
+
+var i = 0;
+//반복문
+while(1){
+    if(i>5){
+        //반복문 탈출
+        break;
+    }
+
+    console.log(i);
+    i++;
+}
+
---
+++ Reservation/Javascript/Basics/5.5/Object.js
@@ -0,0 +1,12 @@
+var person={
+    name: 'Lee',
+    gender: 'male',
+    sayHello: function(){
+        console.log('Hi! My name is ' + this.name);
+    }
+};
+
+console.log(typeof person);
+console.log(person);
+
+person.sayHello();
